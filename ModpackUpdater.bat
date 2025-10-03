b64522957fd35141afed46f3b62521c134936774
Metehan <gliencherth@gmail.com>
2025-10-03 18:56:28 +0300

    Regular-Auto-Sync

--- ModpackUpdater.bat
+++ ModpackUpdater.bat
@@ -4,9 +4,13 @@ REM === Değişiklikleri ekle ve commit et ===
 git add .
 git commit -m "Regular-Auto-Sync"
 
+pause
+
 REM === Uzak repo ile senkronize ol ===
 git pull origin main
 git push origin main
+
+
 
 REM === TLauncher’ı çalıştır ===
 start "" "C:\Users\Ciaodar\AppData\Roaming\.minecraft\TLauncher.exe"
